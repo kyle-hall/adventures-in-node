c5adb7b43c766fe98b81a16c4dbfa2a1ea994fac
Kyle Hall <hallkd47@gmail.com>
2016-01-07 23:38:53 -0600

    adds code to render handlebars pages and server static files

--- express-book/meadowlark-project/meadowlark.js
+++ express-book/meadowlark-project/meadowlark.js
@@ -9,28 +9,26 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send("Meadowlark Travel");
+  res.render('index');
 });
 
 app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send("About Meadowlark Travel");
+  res.render('about');
 });
 
 //Custom 404 page
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send("404 - Page Not Found");
+  res.render('404');
 });
 
 //Custom 500 page
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status(500);
-  res.send("500 - Internal Server Error");
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
