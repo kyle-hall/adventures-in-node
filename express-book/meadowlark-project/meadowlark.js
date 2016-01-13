6abd43cbb8291f3e9850b978135c354d5d59720f
Kyle Hall <hallkd47@gmail.com>
2016-01-12 21:06:29 -0600

    adds form route to accept user emails

--- express-book/meadowlark-project/meadowlark.js
+++ express-book/meadowlark-project/meadowlark.js
@@ -1,6 +1,8 @@
 
 var express = require('express'),
-      app = express();
+      app = express(),
+      fortunes = require('./lib/fortune'),
+      bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -9,6 +11,13 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlEncoded({"extended": true}));
+
+app.use(function (req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -16,7 +25,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-  res.render('about');
+  res.render('about', {
+    fortune: fortunes.getFortune(),
+    pageTestScript: 'tests/tests-about.js'
+  });
+});
+
+app.get('/newsletter', function(req, res) {
+  res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.post('process', function (req, res) {
+  console.log("Form (from querystring): " + req.query.form);
+  console.log("CSRF token (from hidden form field): " + req.body._csrf);
+  console.log("Name (from visible form field): " + req.body.name);
+  console.log("Email (from visible form field): " + req.body.email);
+  res.redirect(303, '/thank-you');
 });
 
 //Custom 404 page
