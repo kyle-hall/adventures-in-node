e14548ba7f23796842ae14a2ac00d41aae4ceb38
Kyle Hall <hallkd47@gmail.com>
2016-01-12 21:08:33 -0600

    starting to work on concatenating all files in a directory

--- myConcat/app.js
+++ myConcat/app.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 var fs = require('fs');
 
+if (process.argv[2][0] === "-") {
+  //check if it's the directory flag
+  if (process.argv[2][1] === "d") {
+    //concatenate all files in process.argv[4] to process.argv[3]
+    //read the directory and then asynchronously read each one
+    //once all of the files have been read, concatenate them, one by one
+  }
+}
+
 fs.readFile(process.argv[2], "utf8", function (err, data) {
   if (err) throw err;
 
